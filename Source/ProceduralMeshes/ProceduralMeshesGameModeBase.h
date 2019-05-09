26d4b3a5cc501eeb98308b07973ff30b050d6f90
unpaleness <generalissimusunpaleness@gmail.com>
2019-05-09 16:47:17 +0300

    Init commit

---
+++ Source/ProceduralMeshes/ProceduralMeshesGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralMeshesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALMESHES_API AProceduralMeshesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
