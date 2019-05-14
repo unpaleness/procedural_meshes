64eb28a8b351bd829f812a17bf7d8685e8fad65c
unpaleness <generalissimusunpaleness@gmail.com>
2019-05-14 03:00:16 +0300

    Generator broken

--- Source/ProceduralMeshes/Maze.cpp
+++ Source/ProceduralMeshes/Maze.cpp
@@ -8,7 +8,7 @@ void AMaze::BeginPlay() {
 }
 
 bool AMaze::HasChanges() {
-	bool result { false };
+	bool result{ false };
 	if (Super::HasChanges()) {
 		result = true;
 	}
@@ -30,7 +30,7 @@ void AMaze::InitArrays() {
 	H = Height;
 
 	// One cell's size
-	float CellSize { Y >= X ? InnerSize / (W * (Y + 1) + Y) : InnerSize / (W * (X + 1) + X) };
+	float CellSize{ Y >= X ? InnerSize / (W * (Y + 1) + Y) : InnerSize / (W * (X + 1) + X) };
 
 	// Add floor
 	AddPlane(
@@ -100,7 +100,7 @@ void AMaze::InitArrays() {
 }
 
 void AMaze::AddPlane(FVector P1, FVector P2) {
-	int32 V { Vertices.Num() };
+	int32 V{ Vertices.Num() };
 
 	FVector V0(P1);
 	FVector V1(P2.X, P1.Y, (P1.Z + P2.Z) / 2.0f);
@@ -122,7 +122,7 @@ void AMaze::AddPlane(FVector P1, FVector P2) {
 }
 
 void AMaze::AddCuboid(FVector P1, FVector P2, EMazeCubiodFaces Direction) {
-	int32 V { Vertices.Num() };
+	int32 V{ Vertices.Num() };
 
 	FVector V0(P1);
 	FVector V1(P1.X, P1.Y, P2.Z);
@@ -193,6 +193,46 @@ void AMaze::GenerateMaze() {
 	HWalls.Init(true, (Y + 1) * X);
 	VWalls.Init(true, Y * (X + 1));
 
+	// Temporary cell state:
+	//   false - not visited
+	//   true  - visited
+	bool* Cells = new bool[X * Y];
+	for (int32 i = 0; i < X * Y; ++i) {
+		Cells[i] = false;
+	}
+	// Path of generator
+	int32* Path = new int32[X * Y];
+	// Elements in path
+	int32 N{ 1 };
+
+	// Start with random cell
+	Path[0] = rand() % (X * Y);
+	Cells[Path[0]] = true;
+
+	int32 it{ 0 };
+	while (N >= 0) {
+		if (++it > 10000) {
+			UE_LOG(LogProceduralMeshes, Warning, TEXT("%s: Limit of 10000 iteration exceeded!"), *GetName());
+			break;
+		}
+		UE_LOG(LogProceduralMeshes, Display, TEXT("%s: Processing (%i, %i)"), *GetName(), Path[N - 1] % X, Path[N - 1] / X);
+		int32 NextCell{ GetRandomNeighbour(Path[N - 1], Cells) };
+		if (NextCell >= X * Y) {
+			UE_LOG(LogProceduralMeshes, Warning, TEXT("%s: Next cell is %i!"), *GetName(), NextCell);
+			break;
+		} else {
+			UE_LOG(LogProceduralMeshes, Display, TEXT("%s: Next cell is %i"), *GetName(), NextCell);
+		}
+		if (NextCell > 0) {
+			Path[N] = NextCell;
+			BreakWall(Path[N - 1], Path[N]);
+			Cells[N] = true;
+			++N;
+		} else {
+			--N;
+		}
+	}
+
 	//// Fill walls
 	//for (int32 j = 0; j < Y + 1; ++j) {
 	//	for (int32 i = 0; i < X + 1; ++i) {
@@ -206,4 +246,67 @@ void AMaze::GenerateMaze() {
 	//}
 
 	IsMazeNeedGenerate = false;
+
+	delete[] Path;
+	delete[] Cells;
+}
+
+int32 AMaze::GetRandomNeighbour(int32 Index, bool* Cells) {
+	// 1 - North, 2 - South, 4 - West, 8 - East
+	uint8_t DirFlags{ 0 };
+	uint8_t Dirs{ 0 };
+	// Northern edge
+	if (Index >= 40 && !Cells[Index - X]) {
+		DirFlags += 1;
+		++Dirs;
+	}
+	// Southern edge
+	if (Index < (Y - 1) * X && !Cells[Index + X]) {
+		DirFlags += 2;
+		++Dirs;
+	}
+	// Western edge
+	if (Index % X != 0 && !Cells[Index - 1]) {
+		DirFlags += 4;
+		++Dirs;
+	}
+	// Eastern edge
+	if (Index % X != X - 1 && !Cells[Index + 1]) {
+		DirFlags += 8;
+		++Dirs;
+	}
+	UE_LOG(LogProceduralMeshes, Display, TEXT("%s: Neighbours %i"), *GetName(), DirFlags);
+	// No neighbours
+	if (Dirs <= 0) {
+		return -1;
+	}
+	uint8_t Dir = rand() % Dirs;
+	uint8_t ValidDir{ 0 };
+	for (uint8_t i = 0; i < 4; ++i) {
+		ValidDir += (DirFlags >> i) % 2;
+		if (ValidDir == Dir) {
+			switch (i) {
+				case 0: return Index - X;
+				case 1: return Index + X;
+				case 2: return Index - 1;
+				case 3: return Index + 1;
+			}
+			break;
+		}
+	}
+	return -1;
+}
+
+void AMaze::BreakWall(int32 C1, int32 C2) {
+	if (C1 < 0 || C2 < 0 || C1 >= X * Y || C2 >= X * Y || (C1 % X == 0 && C2 % X == X - 1) || (C1 % X == X - 1 && C2 % X == 0)) {
+		return;
+	}
+	if (abs(C1 - C2) == 1) {
+		VWalls[C1 / X * (X + 1) + (C1 > C2 ? C1 % X : C2 % X)] = false;
+		return;
+	}
+	if (abs(C1 - C2) == X) {
+		HWalls[(C1 > C2 ? C1 : C2) + C1 % X] = false;
+		return;
+	}
 }
--- Source/ProceduralMeshes/Maze.h
+++ Source/ProceduralMeshes/Maze.h
@@ -25,7 +25,21 @@ protected:
 
 	void AddPlane(FVector P1, FVector P2);
 	void AddCuboid(FVector P1, FVector P2, EMazeCubiodFaces Direction);
+	/** Generates maze using Recursive Backtracking algorithm */
 	void GenerateMaze();
+	/**
+	 * Gets index of not visited neighbour cell
+	 * @param Index                Index of current cell
+	 * @param Cells                Array of cells, where "true" - visited, "false" - unvisited
+	 * @return Index of not visited neighbour cell or -1 if where is no such cell
+	 */
+	int32 GetRandomNeighbour(int32 Index, bool* Cells);
+	/**
+	 * Tries to break a wall between two cells
+	 * @param C1                   Index of first cell
+	 * @param C2                   Index of second cell
+	 */
+	void BreakWall(int32 C1, int32 C2);
 
 protected:
 	/** Number of cells in horizontal direction */
